1c6d8f919025d3a48f8774a2481de5e444f504eb
Angel Andrade <aandradeb99@gmail.com>
2025-11-20 12:55:29 +0100

    Create basic website design

---
+++ src/ui/config/sidebar.py
@@ -0,0 +1,26 @@
+sidebar_items = [
+    {
+        "title": "Overview",
+        "href": "/dashboard",
+        "icon": "Overview Icon",
+        "image": "images/icons/overview.svg"
+    },
+    {
+        "title": "Workouts",
+        "href": "#",
+        "icon": "Workouts Icon",
+        "image": "images/icons/workouts.svg"
+    },
+    {
+        "title": "Programs",
+        "href": "/programs",
+        "icon": "Programs Icon",
+        "image": "images/icons/programs.svg"
+    },
+    {
+        "title": "Body Weights",
+        "href": "#",
+        "icon": "Weights Icon",
+        "image": "images/icons/body-weights.svg"
+    },
+]
