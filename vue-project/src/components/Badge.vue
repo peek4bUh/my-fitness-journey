19a5b07c89cb32595460ae6a71010fff68d4d8dd
Angel Andrade <aandradeb99@gmail.com>
2025-12-09 12:56:31 +0100

    Create default vue structure

---
+++ vue-project/src/components/Badge.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <span class="px-4 py-2 border border-gray-300 rounded-full">
+    {{ msg }}
+  </span>
+</template>
