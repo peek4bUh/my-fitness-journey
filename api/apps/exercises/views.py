6ebf91da07e1c83d0d07864d3966b2a2f00abc0f
Angel Andrade <aandradeb99@gmail.com>
2026-04-08 10:33:29 +0200

    chore: remove admin panel

--- api/apps/exercises/views.py
+++ api/apps/exercises/views.py
@@ -1,17 +1,17 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import AllowAny
 
 from apps.exercises.serializers import ExerciseSerializer
 from apps.exercises.models import Exercise
 
 
-class ExerciseList(ListAPIView):
+class ExerciseList(ListCreateAPIView):
     permission_classes = [AllowAny]
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
 
 
-class ExerciseDetail(ListAPIView):
+class ExerciseDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = [AllowAny]
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
--- api/config/settings.py
+++ api/config/settings.py
@@ -35,12 +35,9 @@ DEBUG = True
 # Installed apps in the project
 # https://docs.djangoproject.com/en/6.0/ref/settings/#installed-apps
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework',
     'apps.users',
     'apps.programs',
@@ -59,24 +56,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-# https://docs.djangoproject.com/en/6.0/topics/templates/
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
 ]
 
 # Django REST Framework
