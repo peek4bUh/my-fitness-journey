a269662d99523a15b70d0c295c3fc17a4e55bb86
Angel Andrade <aandradeb99@gmail.com>
2026-04-08 10:14:08 +0200

    chore: remove static_files path

--- api/config/settings.py
+++ api/config/settings.py
@@ -122,12 +122,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
