9f0ede62f64792aa7eb48487af3fa149eac1de1e
Angel Andrade <aandradeb99@gmail.com>
2026-04-10 21:40:18 +0200

    Remove trailing slash in exercise urls

--- api/apps/exercises/urls.py
+++ api/apps/exercises/urls.py
@@ -3,13 +3,13 @@ from apps.exercises import views
 
 urlpatterns = [
     # Exercises
-    path("exercises/", views.ExerciseList.as_view(), name="exercise-list"),
-    path("exercises/<int:pk>/", views.ExerciseDetail.as_view(),
+    path("exercises", views.ExerciseList.as_view(), name="exercise-list"),
+    path("exercises/<int:pk>", views.ExerciseDetail.as_view(),
          name="exercise-detail"),
 
     # Difficulties
-    path("difficulties/", views.ExerciseDifficultyList.as_view(),
+    path("difficulties", views.ExerciseDifficultyList.as_view(),
          name="difficulty-list"),
-    path("difficulties/<int:pk>/",
+    path("difficulties/<int:pk>",
          views.ExerciseDifficultyDetail.as_view(), name="difficulty-detail"),
 ]
