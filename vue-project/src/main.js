91dafa898a9215d8617a8c35ea629da8b9e3da3b
Angel Andrade <aandradeb99@gmail.com>
2026-04-11 21:21:50 +0200

    Add Noir preset and get programs url

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,6 +4,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
+import { definePreset } from '@primeuix/themes'
 import router from './router'
 import { useAuth } from './composables/useAuth'
 import Aura from '@primeuix/themes/aura'
@@ -12,14 +13,61 @@ import axios from 'axios'
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = 'http://localhost:8000/api/v1/'
 
+const Noir = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{zinc.50}',
+      100: '{zinc.100}',
+      200: '{zinc.200}',
+      300: '{zinc.300}',
+      400: '{zinc.400}',
+      500: '{zinc.500}',
+      600: '{zinc.600}',
+      700: '{zinc.700}',
+      800: '{zinc.800}',
+      900: '{zinc.900}',
+      950: '{zinc.950}',
+    },
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{zinc.950}',
+          inverseColor: '#ffffff',
+          hoverColor: '{zinc.900}',
+          activeColor: '{zinc.800}',
+        },
+        highlight: {
+          background: '{zinc.950}',
+          focusBackground: '{zinc.700}',
+          color: '#ffffff',
+          focusColor: '#ffffff',
+        },
+      },
+      dark: {
+        primary: {
+          color: '{zinc.50}',
+          inverseColor: '{zinc.950}',
+          hoverColor: '{zinc.100}',
+          activeColor: '{zinc.200}',
+        },
+        highlight: {
+          background: 'rgba(250, 250, 250, .16)',
+          focusBackground: 'rgba(250, 250, 250, .24)',
+          color: 'rgba(255,255,255,.87)',
+          focusColor: 'rgba(255,255,255,.87)',
+        },
+      },
+    },
+  },
+})
+
 const app = createApp(App)
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: Noir,
     options: {
       prefix: 'p',
       darkModeSelector: false,
-      cssLayer: false,
     },
   },
 })
--- vue-project/src/views/dashboard/ProgramsView.vue
+++ vue-project/src/views/dashboard/ProgramsView.vue
@@ -11,7 +11,7 @@ const error = ref(null)
 
 onMounted(async () => {
   try {
-    const response = await axios.get('http://127.0.0.1:8000/programs/')
+    const response = await axios.get('programs')
     programs.value = response.data
     console.log(response.data)
   } catch (err) {
