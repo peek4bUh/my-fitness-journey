3d035231cfc9f914b96e61288861fed7eaa9a6ad
Angel Andrade <aandradeb99@gmail.com>
2026-04-20 18:46:03 +0200

    chore: add prettier-plugin-organize-imports and config eslint

--- vue-project/eslint.config.js
+++ vue-project/eslint.config.js
@@ -1,22 +1,27 @@
+import js from '@eslint/js'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVue from 'eslint-plugin-vue'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default defineConfig([
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
-  },
-
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   {
+    name: 'app/main-config',
+    files: ['**/*.{js,vue}'],
     languageOptions: {
       globals: {
         ...globals.browser,
       },
+    },
+    rules: {
+      'vue/block-order': [
+        'error',
+        {
+          order: ['script', 'template', 'style'],
+        },
+      ],
     },
   },
 
